a396846c2d83112b21dcf91ce4540da735374bf1
Eduardo Vital <vitaldu@gmail.com>
2016-12-07 12:27:45 -0200

    Fixing redirects in development environment

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ let app = express();
 const port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 })
 
